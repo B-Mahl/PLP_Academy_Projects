02553703dab77f5c4a8bfbc9e478a5478b44e223
B-Mahl <banelemahlangu@gmail.com>
2024-02-26 02:35:09 +0200

    assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+
+print(f'Hi, {name}! Your age is {age} and you live in {location}.')
+
+
